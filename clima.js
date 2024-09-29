103dfae5ed42c902cbd8497587bb71c2aa4592f0
luccaleca <luccaprogramacao@gmail.com>
2024-09-29 16:49:47 -0300

    Adiciona script para obter coordenadas e condições climáticas

---
+++ clima.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const axios = require('axios');
+const readline = require('readline');
+
+const chaveApi = process.env.API_KEY;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const obterCoordenadas = async (cidade) => {
+  try {
+    const resposta = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+      params: {
+        q: cidade,
+        appid: chaveApi,
+        lang: 'pt_br'
+      }
+    });
+
+    const dados = resposta.data;
+    console.log(`As coordenadas de ${dados.name} são:`);
+    console.log(`Latitude: ${dados.coord.lat}`);
+    console.log(`Longitude: ${dados.coord.lon}`);
+  } catch (erro) {
+    console.error('Erro ao buscar os dados climáticos:', erro.message);
+  }
+};
+
+
+const obterCondicoesAtuais = async(lat, lon) => {
+    try{
+        const resposta = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+            params:{
+                lat:lat,
+                lon:lon,
+                appid:chaveApi,
+                lang:'pt_br',
+                units:'metric' //serve para puxar a temperatura em graus celsius
+            }
+        });
+
+        const dados = resposta.data;
+        console.log(`Sensação térmica: ${dados.main.feels_like}ºC`);
+        console;log(`Descrição: ${dados.weather[0].description}`);
+    } catch (erro) {
+        console.error('Erro ao buscar as condições climaticas atuais', erro.message)
+    }
+}
+
+rl.question('Digite o nome de uma cidade: ', (cidade) => {
+  obterCoordenadas(cidade);
+  rl.close();
+});
